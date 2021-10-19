22cd9534674cdbe5375780b0dc84c79df3eaa7d8
Dennis Ploeger <develop@dieploegers.de>
2021-10-19 15:03:58 +0200

    feat: Dialog exporter

--- addons/parrot/plugin.gd
+++ addons/parrot/plugin.gd
@@ -6,6 +6,9 @@ extends EditorPlugin
 # The importer scene
 var _importer: Importer
 
+# The exporter scene
+var _exporter: Exporter
+
 
 # Add the ParrotDialog scene singleton and the import menu button
 func _enter_tree():
@@ -16,16 +19,25 @@ func _enter_tree():
 	add_tool_menu_item("Import Dialog", self, "_on_import_menu_clicked")
 	_importer = preload("res://addons/parrot/nodes/importer.tscn").instance()
 	add_child(_importer)
+	add_tool_menu_item("Export Dialog", self, "_on_export_menu_clicked")
+	_exporter = Exporter.new()
+	get_editor_interface().get_editor_viewport().add_child(_exporter)
 
 
 # remove the singleton and the import menu button
 func _exit_tree():
 	remove_autoload_singleton("Parrot")
 	remove_tool_menu_item("Import Dialog")
+	remove_tool_menu_item("Export Dialog")
 	remove_child(_importer)
+	remove_child(_exporter)
 
 
 # Show the importer on the tool menu
 func _on_import_menu_clicked(_ud):
 	_importer.start_import()
 	
+
+# Show the exporter on the tool menu
+func _on_export_menu_clicked(_ud):
+	_exporter.start_export()
