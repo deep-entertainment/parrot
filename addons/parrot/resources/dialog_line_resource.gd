1d3b39e37d194876f79a3f2b17e51b20d57c55d6
Dennis Ploeger <develop@dieploegers.de>
2022-07-04 22:40:51 +0200

    fix: Compatiblilty with Godot 3.4 achieved
    fixes deep-entertainment/issues#8

--- addons/parrot/resources/dialog_line_resource.gd
+++ addons/parrot/resources/dialog_line_resource.gd
@@ -6,38 +6,12 @@ extends Resource
 
 
 # The character speaking
-var character: Resource
+export(Resource) var character: Resource
 
 
 # The text the character says
-var text: String
+export(String, MULTILINE) var text: String
 
 
 # An image that is displayed full screen while the line is shown
-var image: Texture
-
-
-# Build the property list
-#
-# ** Returns **
-# - The list of properties
-func _get_property_list() -> Array:
-	var properties = []
-	properties.append({
-		name = "character",
-		type = TYPE_OBJECT,
-		hint = PROPERTY_HINT_RESOURCE_TYPE,
-		hint_string = "CharacterResource"
-	})
-	properties.append({
-		name = "text",
-		type = TYPE_STRING,
-		hint = PROPERTY_HINT_MULTILINE_TEXT,
-	})
-	properties.append({
-		name = "image",
-		type = TYPE_OBJECT,
-		hint = PROPERTY_HINT_RESOURCE_TYPE,
-		hint_string = "Texture"
-	})
-	return properties
+export(Texture) var image: Texture
